83fd750d3e29e0c9841fe8f1a434faf751c124c4
jaguar6105 <gilljoseph603@gmail.com>
2021-01-14 14:06:47 -0500

    add table.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Table from "./components/Table";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-
-    </div>
+    <Table/>
   );
 }
 
